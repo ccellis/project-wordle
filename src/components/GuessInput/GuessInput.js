e7b352e397b8bf9fda6156daedff19cd8703f067
Coleman Ellis <colemancellis@gmail.com>
2023-09-15 15:00:47 -0400

    Add initial guess input

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(guess);
+    setGuess("");
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        required
+        id="guess-input"
+        type="text"
+        pattern="[A-Z]{5}"
+        title="Guess must be exactly 5 letters"
+        value={guess}
+        onChange={(event) => setGuess(event.target.value.toUpperCase())}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
